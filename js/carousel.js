b094e28635b936199fb0104ec18e44b2b1509d12
CosimoA <cosimo.archip@gmail.com>
2023-11-23 17:04:45 +0100

    config: milestones in Js

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+// dichiarare arrey immagini carosello
+
+// dichiaro il nome degli idem dentro al array
+
+// dichiaro i pulsanti di navigazione
+
+// creo evento al click
+
+// se click pulsante next togli la classe active dall'item
+
+// incrementiamo l'indice di selezione dell'item
+
+// assegno la classe item all'elemento corrispondente all'indice
+
+// se gli item array sono finiti nascondere pulsante next
+
+// ripeto il processo al contrario per pulsante prev

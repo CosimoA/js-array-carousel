c5f60ec5ae5183d0b8c2ad443fd4f506bf12cc8e
CosimoA <cosimo.archip@gmail.com>
2023-11-23 17:16:17 +0100

    feature: add forword button function

--- js/carousel.js
+++ js/carousel.js
@@ -1,17 +1,30 @@
 // dichiarare arrey immagini carosello
+const items = document.getElementsByClassName("item");
+console.log(items);
 
 // dichiaro il nome degli idem dentro al array
+let activeImg = 0;
 
 // dichiaro i pulsanti di navigazione
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
 
-// creo evento al click
+// creo evento al click per pulsante next
 
-// se click pulsante next togli la classe active dall'item
-
-// incrementiamo l'indice di selezione dell'item
-
-// assegno la classe item all'elemento corrispondente all'indice
-
-// se gli item array sono finiti nascondere pulsante next
+next.addEventListener("click", function(){
+    if (activeImg < items.length -1) {
+        // se click pulsante next togli la classe active dall'item
+        items[activeImg].classList.remove("active");
+        // incrementiamo l'indice di selezione dell'item
+        activeImg++;
+        // assegno la classe item all'elemento corrispondente all'indice
+        items[activeImg].classList.add("active");
+        // se gli item array sono finiti nascondere pulsante next
+        if (activeImg === items.length -1) {
+            next.classList.add("hidden");
+        }
+    }
+});
 
 // ripeto il processo al contrario per pulsante prev
+
